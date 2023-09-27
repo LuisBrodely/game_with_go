247889775a6e35e55b3483930aced6ac4e61a396
Luis Brodely Tovar Escobar <ltovar@neodata.com.mx>
2023-09-27 02:47:43 -0600

    mejorando funcionalidad

--- screens/GameScene.go
+++ screens/GameScene.go
@@ -3,17 +3,19 @@ package scenes
 import (
     "SimpsonsGame/driver"
     "SimpsonsGame/models"
-    "time"
-
     "fyne.io/fyne/v2"
     "fyne.io/fyne/v2/canvas"
     "fyne.io/fyne/v2/container"
     "fyne.io/fyne/v2/storage"
     "fyne.io/fyne/v2/widget"
+    "time"
+    "strconv" // Importar el paquete strconv para convertir el puntaje a cadena
 )
 
 type GameScene struct {
     window fyne.Window
+    score  int // Variable para llevar el conteo de puntos
+    scoreLabel *widget.Label // Label para mostrar el puntaje
 }
 
 var t *models.Homero
@@ -22,6 +24,7 @@ var c *driver.CollisionDriver
 
 func NewGameScene(window fyne.Window) *GameScene {
     scene := &GameScene{window: window}
+    scene.score = 0 // Inicializa el conteo de puntos
     scene.Render()
     scene.StartGame()
     return scene
@@ -31,63 +34,90 @@ func (s *GameScene) Render() {
     backgroundImage := canvas.NewImageFromURI(storage.NewFileURI("./assets/moe.png"))
     backgroundImage.Resize(fyne.NewSize(800, 600))
     backgroundImage.Move(fyne.NewPos(0, 0))
-    // Models Render
+
     HomeroPeel := createPeel("./assets/homero.png", 100, 100, 100, 450)
     t = models.NewHomero(350, 450, HomeroPeel)
 
     DonaPeel := createPeel("./assets/dona.png", 100, 100, 100, 50)
     w = models.NewDona(350, 600, DonaPeel, t)
 
-    //CollisionDriver
     c = driver.NewCollisionDriver(t, w)
 
-    // Buttons Render
     btnLeft := widget.NewButton("<", t.GoLeft)
-    btnLeft.Resize(fyne.NewSize(50, 50))
-    btnLeft.Move(fyne.NewPos(350, 550))
+    btnLeft.Resize(fyne.NewSize(80, 80))
+    btnLeft.Move(fyne.NewPos(0, 520))
 
     btnRight := widget.NewButton(">", t.GoRight)
-    btnRight.Resize(fyne.NewSize(50, 50))
-    btnRight.Move(fyne.NewPos(400, 550))
+    btnRight.Resize(fyne.NewSize(80, 80))
+    btnRight.Move(fyne.NewPos(720, 520))
 
-    // Registrar el manejador de eventos de teclado
+    // Agregar el marcador de puntuación
+    s.scoreLabel = widget.NewLabel("Score: 0")
+    s.scoreLabel.Move(fyne.NewPos(10, 10))
+
     s.window.Canvas().SetOnTypedKey(s.handleArrowKeys)
 
-    s.window.SetContent(container.NewWithoutLayout(backgroundImage, HomeroPeel, DonaPeel, btnLeft, btnRight))
+    s.window.SetContent(container.NewWithoutLayout(
+        backgroundImage,
+        HomeroPeel,
+        DonaPeel,
+        btnLeft,
+        btnRight,
+        s.scoreLabel, // Agregar el marcador al contenido
+    ))
 }
 
-// handleArrowKeys maneja eventos de teclado para mover al personaje.
 func (s *GameScene) handleArrowKeys(ev *fyne.KeyEvent) {
     switch ev.Name {
     case "Left":
-        t.GoLeft() // Llama a la función GoLeft() en tu objeto de jugador (t)
+        t.GoLeft()
     case "Right":
-        t.GoRight() // Llama a la función GoRight() en tu objeto de jugador (t)
+        t.GoRight()
     }
 }
 
 func (s *GameScene) StartGame() {
+    go s.countScore()
     go t.Run()
     go w.Run()
     go c.Run()
-    go s.checkGameOver() 
+    go s.checkGameOver()
 }
 
-func (s *GameScene) StopGame() {
-    t.SetRunning(!t.GetRunning())
-    w.SetRunning(!w.GetRunning())
+func (s *GameScene) incrementScore() {
+    s.score++
+    s.updateScoreLabel() // Actualizar el texto del marcador
+}
+
+func (s *GameScene) getScore() int {
+    return s.score
+}
+
+func (s *GameScene) countScore() {
+    ticker := time.NewTicker(time.Second)
+    defer ticker.Stop()
+    for {
+        select {
+        case <-ticker.C:
+            s.incrementScore()
+        }
+    }
 }
 
 func (s *GameScene) checkGameOver() {
     running := true
     for running {
-        if c.GetGameOver() {
+        if c.GetGameOver() || s.getScore() >= 50 { // Cambia 10 a la cantidad deseada de puntos para mostrar Game Over
             running = false
             time.Sleep(2000 * time.Millisecond)
             NewGameOverScene(s.window)
-
         }
     }
+}
+
+func (s *GameScene) updateScoreLabel() {
+    scoreStr := strconv.Itoa(s.getScore())
+    s.scoreLabel.SetText("Score: " + scoreStr)
 }
 
 func createPeel(fileURI string, sizeX float32, sizeY float32, posX float32, posY float32) *canvas.Image {
